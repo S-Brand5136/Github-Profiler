013c494f7e02f77cd8cd7fb88fc57c73f624aef1
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-15 22:45:03 -0600

    Add logout button to nav, and welcome on login

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+
+    public function destroy()
+    {
+        Auth::logout();
+
+        return redirect('/')->with('sucess', 'Goodbye');
+    }
+}
