3edbae68c8023bd19e96ae5cafcdaf0485d854dc
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-15 18:02:08 -0600

    Add Register page, with controller

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,24 @@
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
 <body style="font-family: Open Sans, sans-serif">
+    <section class="px-6 py-8">
+        <nav class="md:flex md:justify-between md:items-center">
+            <div>
+                <a href="/">
+                    <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
+                </a>
+            </div>
+
+            <div class="mt-8 md:mt-0">
+                <a href="/" class="text-xs font-bold uppercase">Home Page</a>
+
+                <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+                    Subscribe for Updates
+                </a>
+            </div>
+        </nav>
+    </section>
+
         {{ $slot }}
 
         <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,40 @@
+<x-layout>
+    <section class="px-6 py-8">
+        <main class="max-w-lg mt-10 mx-auto bg-gray-100 border border-gray-200 p-6 rounded-xl">
+            <h1 class="text-center font-bold text-xl">Register!</h1>
+            <form action="/register" method="POST" class="mt-10">
+                @csrf
+                <div class="mb-6">
+                    <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">Name</label>
+
+                    <input type="text" name="name" id="name" required class="border border-gray-400 p-2 w-full">
+                </div>
+
+                <div class="mb-6">
+                    <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">Username</label>
+
+                    <input type="text" name="username" id="username" required class="border border-gray-400 p-2 w-full">
+                </div>
+
+                <div class="mb-6">
+                    <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">Email</label>
+
+                    <input type="text" name="email" id="email" required class="border border-gray-400 p-2 w-full">
+                </div>
+
+                <div class="mb-6">
+                    <label for="password" class="block mb-2 uppercase font-bold text-xs text-gray-700">Password</label>
+
+                    <input type="password" name="password" id="password" required class="border border-gray-400 p-2 w-full">
+                </div>
+
+                <div class="mb-6">
+                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+                        Submit
+                    </button>
+                </div>
+
+            </form>
+        </main>
+    </section>
+</x-layout>
